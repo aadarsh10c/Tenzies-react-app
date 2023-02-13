1e808dcacda30f20888826d689b287c949c52758
Adarsh Kumar <adarsh010c@gmail.com>
2023-02-13 21:17:05 +0530

    Added dice_array state, and imported nanoid

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Tile(props){
     return(
-        <p className="tile">
-            {props.value}
+        <p className="tile" onClick={props.handleClick}>
+            {props.dice.value}
         </p>
     )
 }
--- src/components/TileGroup.jsx
+++ src/components/TileGroup.jsx
@@ -1,9 +1,46 @@
 import React from "react"
+import { nanoid } from 'nanoid'
 import Tile from './Tile'
 
+function intializer(){
+    let randomValueArray = Array.from(
+        {length:10},() => Math.floor( Math.random() * ( 11 -1)+1) 
+        )
+    
+    return randomValueArray.map( (value , index) => ({
+        id:nanoid(10),
+        isSet:false,
+        value:value
+    }))
+    
+}
+
+
 export default function TileGroup(){
-    const [ values , setValues ] = React.useState(() => Array.from({length:10},() => Math.floor( Math.random() * ( 11 -1)+1) ))
-    const tile_array = values.map( (value) => <Tile value={value}/> )
+    const [ dice_array , setDice_array ] = React.useState(() => intializer() )
+
+    function handleClick(){
+        setDice_array( prevValue => {
+            return prevValue.map( value =>(
+                {
+                    ...value,
+                    isSet:!value.isSet
+                }
+            ) )
+        })
+    }
+
+    console.log( dice_array )
+
+    const tile_array = dice_array.map( (value) => (
+        <Tile 
+        key={value.id} 
+        dice={value}
+        handleClick = {handleClick}
+        />) )
+
+    
+    
     return(
         <main className="tile-group">
             {tile_array}
