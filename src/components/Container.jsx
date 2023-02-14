fa787c79acc22f2bff5ba5ca379d06d2a0815d03
Adarsh Kumar <adarsh010c@gmail.com>
2023-02-15 02:20:09 +0530

    Added useEffect hook , improved logic

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,10 +5,10 @@ import TileGroup from './TileGroup'
 
 function intializer(){
     let randomValueArray = Array.from(
-        {length:10},() => Math.floor( Math.random() * ( 11 -1)+1) 
+        {length:10},() => Math.floor( Math.random() * ( 7 -1)+1) 
         )
     
-    return randomValueArray.map( (value , index) => ({
+    return randomValueArray.map( (value) => ({
         id:nanoid(10),
         isSet:false,
         value:value
@@ -20,42 +20,38 @@ export default function Container (){
 
     const [ dice_array , setDice_array ] = React.useState(() => intializer() )
     const [ target, setTarget ] = React.useState(-1)
-    const [counter, setCounter ] = React.useState(0)
     const [ moves, setMoves ] = React.useState(0)
+    const [ tenzies , setTenzies] = React.useState( false )
+
+    React.useEffect( () => {
+        console.log('inside useeffect')
+        const allSet = dice_array.every( die => die.isSet)
+        let target = dice_array[0].value
+        const allSame  = dice_array.every( die => die.value === target)
+        console.log( `allSet: ${allSet} allSame: ${allSame}`)
+        allSet && allSame && setTenzies( true )
+
+    },[dice_array])
+
     function handleClick( event, id ){
+        //console.log("clicked")
         let currentTarget = event.target.textContent
-        if( (counter == 0) || (currentTarget == target )){
-
-            setDice_array( preValues => {
-                return preValues.map( value => {
-                    if( value.id === id ){
-                        return (
-                            {
-                                ...value,
-                                isSet:!value.isSet
-                            }
-                        )
-                    }else{
-                        return {...value}
-                    }
-                })
-            }  )
-
-            if( target == -1){ //initial value ot be changed with current value
-                setTarget(currentTarget)
-            }
-            
-            setCounter( c => c + 1) //increment counter when clicked value is equal to current target
+        // console.log( target )
+        // console.log( currentTarget )
+        if( (target == -1) || (target == currentTarget )){
+            setDice_array( oldDice => oldDice.map( die => 
+            die.id === id ?
+            {...die, isSet: !die.isSet} :
+            die))
         }
-        
+         
+         //if dice is clicked first time set target value   
+        (target == -1) && setTarget( currentTarget ) 
     }
 
+    const getRandomDie = () => Math.floor( Math.random() * ( 7 - 1 ) + 1)
+
     function  rollDice() {
-       // console.log(" roll dice !")
-        let randomValueArray = Array.from(
-            {length:10},
-            () => Math.floor( Math.random() * ( 11 - 1 ) + 1)
-         )
         // console.log(randomValueArray)
 
          setDice_array( preValues => {
@@ -63,7 +59,7 @@ export default function Container (){
                 if( !item.isSet ){
                     return({
                         ...item,
-                        value: randomValueArray[index]
+                        value: getRandomDie()
                     })
                 }else{
                     return({
@@ -73,7 +69,7 @@ export default function Container (){
             })
          })
 
-         if( counter > 0){
+         if( target != -1){
             setMoves( m => m + 1 )
          }
     }
@@ -81,36 +77,58 @@ export default function Container (){
     function reset () {
         setDice_array( () => intializer() )
         setTarget( -1 )
-        setCounter( 0 )
         setMoves( 0 )
+        setTenzies( false)
     }
 
 
 
    // console.log( dice_array )
-
-    const rollBtn = (() => {
-        if( counter < 10 ){
-            return (
-                <a href="#" 
+    const rollBtn = (
+        () => !tenzies ? 
+        (
+            <a href="#" 
                className="roll-btn"
-               onClick={rollDice} 
-               >
-                ROLL
-            </a>
-            )
-        }else{
-            return (
-                <a href="#" 
-               className=" roll-btn roll-btn-reset"
-               onClick={reset} 
-               >
-                Total Moves : {moves}<br />
-                Click to Reset
-            </a>
-            )
-        }
-    })()
+                onClick={rollDice} 
+             >
+            ROLL
+        </a>
+        ) :
+        (
+            <a href="#" 
+                className=" roll-btn roll-btn-reset"
+                onClick={reset} 
+             >
+            Total Moves : {moves}<br />
+            Click to Reset
+        </a>
+        )
+
+    )()
+    
+
+    // const rollBtn = (() => {
+    //     if( counter < 10 ){
+    //         return (
+    //             <a href="#" 
+    //            className="roll-btn"
+    //            onClick={rollDice} 
+    //            >
+    //             ROLL
+    //         </a>
+    //         )
+    //     }else{
+    //         return (
+    //             <a href="#" 
+    //            className=" roll-btn roll-btn-reset"
+    //            onClick={reset} 
+    //            >
+    //             Total Moves : {moves}<br />
+    //             Click to Reset
+    //         </a>
+    //         )
+    //     }
+    // })()
    // console.log( rollBtn )
 
     return(
